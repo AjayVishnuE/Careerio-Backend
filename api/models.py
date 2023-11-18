361600bec97cdb5db14b81eb5ff4be4d697eef5a
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-11-18 14:23:43 +0530

    deploy

--- api/models.py
+++ api/models.py
@@ -26,24 +26,4 @@ class Resume(models.Model):
 
 
 
-class Project(models.Model):
-    title = models.CharField(max_length=100, blank=False)
-    stack = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=100, blank=True)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-
-    def __str__(self):
-        return self.Title
-    
-class Company(models.Model):
-    company_name = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=100, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class ProjectLikes(models.Model):
-    Company_id = models.ForeignKey(Company, on_delete = models.CASCADE, null = True, blank=False)
-    Project_id = models.ForeignKey(Project, on_delete = models.CASCADE, null = True, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
